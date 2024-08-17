811384eaa70386605f55553e41ae08e3af542253
Marcos Vázquez Martín <marcos.vazquez.martin.contractor@bbva.com>
2024-08-17 13:23:51 +0200

    Add different group chats

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, send_from_directory, g, redirect, url_for
+from flask import Flask, render_template, send_from_directory, g, url_for
 from markupsafe import Markup
 import json
-import argparse
 import os
 import re
 
@@ -26,7 +25,6 @@ def replace_image_references(msg_list, search_term):
         except KeyError:
             has_attached_files = False
         if has_attached_files:
-            # not common to have more than 1 attachment
             try:
                 export_file_name = msg["attached_files"][0]["export_name"]
             except KeyError:
@@ -38,6 +36,7 @@ def replace_image_references(msg_list, search_term):
                 count += 1
     return msg_list
 
+
 def replace_html_special_chars(text):
     text = text.replace("<", "&lt;")
     text = text.replace("\u003c", "&lt;")
@@ -47,7 +46,10 @@ def replace_html_special_chars(text):
     return text.replace("\u0026", "&amp;")
 
 
-def load_json_data(group_info_path, messages_path):
+def load_json_data(group_path):
+    group_info_path = os.path.join(group_path, 'group_info.json')
+    messages_path = os.path.join(group_path, 'messages.json')
+
     with open(group_info_path, 'r', encoding='utf-8') as f:
         group_info = json.load(f)
         member_colors = assign_colors(group_info['members'])
@@ -63,8 +65,6 @@ def load_json_data(group_info_path, messages_path):
     return group_info, messages, member_colors
 
 
-# Route to serve media files
-
 @app.template_filter('replace_emoji')
 def replace_emoji(text, annotations):
 
@@ -77,18 +77,18 @@ def replace_emoji(text, annotations):
         if 'custom_emoji_metadata' in annotation:
             shortcode = annotation['custom_emoji_metadata']['custom_emoji']['shortcode']
             content_type = annotation['custom_emoji_metadata']['custom_emoji']['content_type']
-            emoji_url = url_for(
-                'media', filename=f"CustomEmoji-{shortcode[1:-1]}.{content_type.split('/')[-1]}")
+            emoji_url = url_for('media',
+                                group_name=g.group_name,
+                                filename=f"CustomEmoji-{shortcode[1:-1]}.{content_type.split('/')[-1]}")
             text = text.replace(
                 '�', f'<img src="{emoji_url}" alt="{shortcode}" style="width: 30px; height: 30px;">', 1)
-    # Replace URLs with anchor tags
+
     url_pattern = re.compile(r'(https?://\S+)')
     text = url_pattern.sub(r'<a href="\1">\1</a>', text)
 
     code_pattern = re.compile(r'```([^`]+)```', re.DOTALL)
     text = code_pattern.sub(r'<pre><code>\1</code></pre>', text)
 
-    # Replace user mentions with colored text
     mention_pattern = re.compile(
         r'@((?:[\wáéíóúÁÉÍÓÚñÑ]+\s+){0,2}[\wáéíóúÁÉÍÓÚñÑ]+)')
 
@@ -105,46 +105,51 @@ def replace_emoji(text, annotations):
     return Markup(text)
 
 
-@app.route('/media/<path:filename>')
-def media(filename):
-    return send_from_directory(g.group_path, filename)
+@app.route('/media/<group_name>/<path:filename>')
+def media(group_name, filename):
+    return send_from_directory(os.path.join('resources', group_name), filename)
 
-# Route to display group info and messages
 
-@app.route('/<page_name>')
-def index(page_name):
+@app.route('/group/<group_name>')
+def index(group_name):
+    group_path = os.path.join('resources', group_name)
+    if not os.path.exists(group_path):
+        return "Group not found", 404
+
+    group_info, messages, member_colors = load_json_data(group_path)
+
+    # Store the data in Flask's g context for this request
+    g.member_colors = member_colors
+    g.group_path = group_path
+    g.group_info = group_info
+    g.messages = messages
+    g.member_colors = member_colors
+    g.group_name = group_name
+
     return render_template('index.html',
-                           group_info=g.group_info,
-                           messages=g.messages,
-                           member_colors=g.member_colors)
+                           group_info=group_info,
+                           messages=messages,
+                           member_colors=member_colors)
 
 
 @app.route('/')
-def root():
-    return redirect(url_for('index', page_name=app.config['PAGE_NAME']))
-
-
-@app.before_request
-def before_request():
-    g.group_path = app.config['GROUP_PATH']
-    g.group_info = app.config['GROUP_INFO']
-    g.messages = app.config['MESSAGES']
-    g.member_colors = app.config['MEMBER_COLORS']
+def home():
+    groups = []
+    base_path = "resources"
+    for group_name in os.listdir(base_path):
+        group_path = os.path.join(base_path, group_name)
+        if os.path.isdir(group_path):
+            group_info_path = os.path.join(group_path, 'group_info.json')
+            if os.path.exists(group_info_path):
+                with open(group_info_path, 'r', encoding='utf-8') as f:
+                    group_info = json.load(f)
+                    groups.append({
+                        "name": group_info['name'],
+                        "emoji_id": group_info.get('emoji_id', '📁'),
+                        "group_name": group_name
+                    })
+    return render_template('home.html', groups=groups)
 
 
 if __name__ == '__main__':
-
-    group_info, messages, member_colors = load_json_data(
-        "resources/group_info.json", "resources/messages.json")
-
-    # Extract the last part of the group_path
-    page_name = os.path.basename(os.path.normpath("resources"))
-
-    # Store the data in the Flask application config
-    app.config['GROUP_PATH'] = "resources"
-    app.config['GROUP_INFO'] = group_info
-    app.config['MESSAGES'] = messages
-    app.config['MEMBER_COLORS'] = member_colors
-    app.config['PAGE_NAME'] = page_name
-
     app.run()
